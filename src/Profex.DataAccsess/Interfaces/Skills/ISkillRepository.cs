e05f3ed43a3f404fb8d10da2bdc729b34e173eeb
abdisamadovich <islombobokulov143@gmail.com>
2023-08-06 15:14:12 +0500

    SkillRepository yozdim

---
+++ src/Profex.DataAccsess/Interfaces/Skills/ISkillRepository.cs
@@ -0,0 +1,9 @@
+﻿using Profex.DataAccsess.Common;
+using Profex.Domain.Entities.skills;
+
+namespace Profex.DataAccsess.Interfaces.Skills;
+
+public interface ISkillRepository : IRepository<Skill,Skill>, IGetAll<Skill>  
+{
+
+}
---
+++ src/Profex.DataAccsess/Repositories/Skills/SkillRepository.cs
@@ -0,0 +1,117 @@
+﻿using Dapper;
+using Profex.Application.Utils;
+using Profex.DataAccsess.Interfaces.Skills;
+using Profex.Domain.Entities.masters;
+using Profex.Domain.Entities.skills;
+
+namespace Profex.DataAccsess.Repositories.Skills;
+
+public class SkillRepository : BaseRepository, ISkillRepository
+{
+    public async Task<long> CountAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = $"select count(*) from skills";
+            var result = await _connection.QuerySingleAsync<long>(query);
+            return result;
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    public async Task<int> CreateAsync(Skill entity)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "INSERT INTO public.skills(category_id, title, description, created_at, updated_at) " +
+                "VALUES (@CategoryId, @Title, @Description, @CreatedAt, @UpdatedAt);";
+            var result = await _connection.ExecuteAsync(query, entity);
+            return result;
+        }
+        catch { return 0; }
+        finally { await _connection.CloseAsync(); }
+    }
+
+    public async Task<int> DeleteAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = "DELETE FROM skills WHERE id=@Id";
+            var result = await _connection.ExecuteAsync(query, new { Id = id });
+            return result;
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        { await _connection.CloseAsync(); }
+    }
+
+    public async Task<IList<Skill>> GetAllAsync(PaginationParams @params)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = $"SELECT * FROM public.masters ORDER BY id desc offset {@params.SkipCount} limit {@params.PageSize}";
+            var resMas = (await _connection.QueryAsync<Skill>(query)).ToList();
+            return resMas;
+        }
+            catch
+            {
+            return new List<Skill>();
+        }
+            finally
+            { await _connection.CloseAsync(); }
+    }
+
+    public async Task<Skill?> GetByIdAsync(long id)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string qeury = $"SELECT * FROM skills where id=@Id";
+            var res = await _connection.QuerySingleAsync<Skill>(qeury, new { Id = id });
+            return res;
+
+        }
+        catch
+        {
+            return null;
+        }
+        finally { await _connection.CloseAsync(); }
+    }
+
+    public async Task<int> UpdateAsync(long id, Skill entity)
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            string query = $"UPDATE public.skills" +
+                $"SET category_id=@CategoryId, title=@Title, description=@Description, created_at=@CreatedAt, updated_at=@UpdatedAt" +
+                $"WHERE id = {id}";
+            var res = await _connection.ExecuteAsync(query, entity);
+            return res;
+
+
+        }
+        catch
+        {
+            return 0;
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+}
