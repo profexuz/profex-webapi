45035cd57e9b9659a2b3c8c5905c66f60ed7af80
Elbekovich <jumakulovozodbek007@gmail.com>
2023-08-09 10:39:23 +0500

    PostImageRepository is completed

---
+++ src/Profex.DataAccsess/Interfaces/Post_Images/IPostImageRepository.cs
@@ -0,0 +1,8 @@
+﻿using Profex.DataAccsess.Common;
+using Profex.Domain.Entities.post_images;
+
+namespace Profex.DataAccsess.Interfaces.Post_Images
+{
+    public interface IPostImageRepository : IRepository<Post_image, Post_image>, IGetAll<Post_image>
+    {}
+}
