8d4d888a6123801c0a1c99c8df461cb27b01681e
Elbekovich <jumakulovozodbek007@gmail.com>
2023-08-09 23:57:05 +0500

    category test is completed

---
+++ Test/Profex.UnitTest/ValidatorTests/Categories/CategoryCreateValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using Profex.Persistance.Dtos.Categories;
+using Profex.Persistance.Validations.Dtos.Categories;
+
+namespace Profex.UnitTest.ValidatorTests.Categories;
+
+public class CategoryCreateValidatorTest
+{
+    [Theory]
+    [InlineData("1234dsfsdfADASD@#!@#!@#adsasd")]
+    [InlineData("!23aADAdsddsds!@#!@#!@3adsdasd")]
+    [InlineData("!@#QEad123!@#!@#1243123")]
+    [InlineData("A1!@#!#qwesd@#QEsdfsdfsdf")]
+    [InlineData("aAdaE!@qeqWE!@#!@#adsadssa1")]
+    [InlineData("A0as12312da!@#!@#sdsdfsdfsdf")]
+    [InlineData("       dfasdfsdfsdfa12")]
+    [InlineData("       2323411!@#!@aadsfASDA")]
+    [InlineData("       ASD123!@#!@aadsfASDA")]
+    [InlineData("ASD123!@#!@aadsfASDA      asd ")]
+    [InlineData("ASD123!@#!@aadsfASDsdfsA   assdasdas")]
+    [InlineData("       ASD123!@#!@aadsfASDA      asda12asdsdasd")]
+    [InlineData("asd       Aasd0!@#!@aadsfASDA       asasdasd ")]
+    [InlineData("electronic products, we sell an electronic products to our clients, we sell an electronic products to our clients")]
+    public void ShouldReturnInValidValidation(string name)
+    {
+        CategoryCreateDto categoryCreateDto = new CategoryCreateDto()
+        {
+            Name = name,
+            Description = "we sell an electronic products to our clients",
+        };
+        var validator = new CategoryCreateValidator();
+        var result = validator.Validate(categoryCreateDto);
+        Assert.False(result.IsValid);
+    }
+}
