c1ad3f6e44e6bd9fe750f759b8a6d4ebc469f5ca
Elbekovich <jumakulovozodbek007@gmail.com>
2023-08-11 11:12:41 +0500

    MasterCreateValidatorTest is completed

---
+++ src/Profex.Persistance/Dtos/Masters/MasterCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Profex.Persistance.Dtos.Masters;
+
+public class MasterCreateDto
+{
+    public string FirstName { get; set; } =String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string Phone_number { get; set; } = string.Empty;
+    public string Phone_number_confirmed { get; set; } = string.Empty;
+    public IFormFile Image_path { get; set; } = default!;
+    public string Password_hash { get; set; } = string.Empty;
+    public string Salt { get; set; } = string.Empty;
+    public bool Is_free { get; set; }
+}
---
+++ src/Profex.Persistance/Validations/Dtos/Masters/MasterCreateValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Profex.Persistance.Dtos.Masters;
+using Profex.Service.Helpers;
+
+namespace Profex.Persistance.Validations.Dtos.Masters;
+
+public class MasterCreateValidator : AbstractValidator<MasterCreateDto>
+{
+    public MasterCreateValidator()
+    {
+        RuleFor(dto => dto.FirstName).NotEmpty().NotNull().WithMessage("First name is required!")
+            .MaximumLength(20).WithMessage("First name lass be than 20 characters")
+            .MinimumLength(3).WithMessage("First name must be than 3 characters");
+
+        RuleFor(dto => dto.LastName).NotEmpty().NotNull().WithMessage("Last name is required!")
+            .MaximumLength(20).WithMessage("Last name lass be than 20 characters")
+            .MinimumLength(3).WithMessage("Last name must be than 3 characters");
+
+        RuleFor(dto => dto.Phone_number).Must(phone => PhoneNumberValidator.IsValid(phone))
+            .WithMessage("Phone number is invalid! ex: +998xxYYYAABB");
+
+        int maxImageSizeMB = 3;
+        RuleFor(dto => dto.Image_path).NotEmpty().NotNull().WithMessage("Image field is required");
+        RuleFor(dto => dto.Image_path.Length).LessThan(maxImageSizeMB * 1024 * 1024 + 1).WithMessage($"Image size must be less than {maxImageSizeMB} MB");
+        RuleFor(dto => dto.Image_path.FileName).Must(predicate =>
+        {
+            FileInfo fileInfo = new FileInfo(predicate);
+
+            return MediaHelper.GetImageExtensions().Contains(fileInfo.Extension);
+        }).WithMessage("This file type is not image file");
+
+        RuleFor(dto => dto.Password_hash).NotNull().NotEmpty().WithMessage("Password is requuired")
+            .Must(password => PasswordValidator.IsStrongPassword(password).IsValid).WithMessage("Password is not strong password!");
+    }
+}
