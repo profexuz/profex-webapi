95bc7f71588829a3717362b254aa00b7eb9cfd7a
abdisamadovich <islombobokulov143@gmail.com>
2023-08-11 16:32:14 +0500

    WEBAPI : Middleware

---
+++ src/Profex.Application/Exceptions/ClientException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace Profex.Application.Exceptions;
+
+public abstract class ClientException : Exception
+{
+    public abstract HttpStatusCode StatusCode { get; }
+    public abstract string TitleMessage { get; protected set; }
+}
