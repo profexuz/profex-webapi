bededc135f3fd4874d3bd1e94b700468903529e2
Elbekovich <jumakulovozodbek007@gmail.com>
2023-08-19 16:33:45 +0500

    MasterUpdate validator is completed

---
+++ src/Profex.Persistance/Validations/Dtos/Masters/MasterUpdateValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Profex.Persistance.Dtos.Master1;
+using Profex.Service.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profex.Persistance.Validations.Dtos.Masters
+{
+    public class MasterUpdateValidator : AbstractValidator<Master1UpdateDto>
+    {
+        public MasterUpdateValidator()
+        {
+            RuleFor(dto => dto.FirstName).NotEmpty().NotNull().WithMessage("First name is required!")
+            .MaximumLength(20).WithMessage("First name lass be than 20 characters")
+            .MinimumLength(3).WithMessage("First name must be than 3 characters");
+
+            RuleFor(dto => dto.LastName).NotEmpty().NotNull().WithMessage("Last name is required!")
+                .MaximumLength(20).WithMessage("Last name lass be than 20 characters")
+                .MinimumLength(3).WithMessage("Last name must be than 3 characters");
+
+            RuleFor(dto => dto.PhoneNumber).Must(phone => PhoneNumberValidator.IsValid(phone))
+                .WithMessage("Phone number is invalid! ex: +998xxYYYAABB");
+
+            int maxImageSizeMB = 3;
+            RuleFor(dto => dto.ImagePath).NotEmpty().NotNull().WithMessage("Image field is required");
+            RuleFor(dto => dto.ImagePath.Length).LessThan(maxImageSizeMB * 1024 * 1024 + 1).WithMessage($"Image size must be less than {maxImageSizeMB} MB");
+            RuleFor(dto => dto.ImagePath.FileName).Must(predicate =>
+            {
+                FileInfo fileInfo = new FileInfo(predicate);
+
+                return MediaHelper.GetImageExtensions().Contains(fileInfo.Extension);
+            }).WithMessage("This file type is not image file");
+        }
+    }
+}
---
+++ src/Profex.WebApi/Controllers/User/User/UserUpdate/UserUpdateController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Profex.Persistance.Dtos.User1;
+using Profex.Service.Interfaces.User1;
+
+namespace Profex.WebApi.Controllers.User.User.UserUpdate
+{
+    [Route("api/user/update")]
+    [ApiController]
+    public class UserUpdateController : ControllerBase
+    {
+        private readonly IUser1Service _service;
+        public UserUpdateController(IUser1Service service)
+        {
+            _service = service;
+        }
+        [HttpPut("{userId}")]
+
+        public async Task<IActionResult> UpdateAsync(long userId, [FromForm] User1UpateDto dto)
+        {
+
+            /*var updateValidator = new CarUpdateValidator();
+            var result = updateValidator.Validate(dto);
+            if (result.IsValid) return Ok(await _repository.UpdateAsync(carId, dto));
+            else return BadRequest(result.Errors);*/
+
+            return Ok(await _service.UpdateAsync(userId, dto));
+        }
+    }
+}
