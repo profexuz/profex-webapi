625ab062c08b3e616eeb81eb1fbd90a7139cd0c1
temurbek-kdv <temur.kdv@gmail.com>
2023-09-09 09:51:23 +0500

    BaseRepository changed

--- src/Profex.DataAccsess/Repositories/BaseRepository.cs
+++ src/Profex.DataAccsess/Repositories/BaseRepository.cs
@@ -10,6 +10,6 @@ public class BaseRepository
     {
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
-        this._connection = new NpgsqlConnection("Host=localhost; Port=5432; Database=fuck; User Id=postgres; Password=ll;");
+        this._connection = new NpgsqlConnection("Host=db-postgresql-lon1-56814-do-user-14588545-0.b.db.ondigitalocean.com; Port=25060; Database=gettalim-db; User Id=doadmin; Password=AVNS_9n5XkthWFqLIltZLhLQ");
     }
 }
