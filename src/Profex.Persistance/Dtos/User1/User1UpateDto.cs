15c4d59d2f717bed377b4ebf8a9be4964a24d550
temurbek-kdv <temur.kdv@gmail.com>
2023-09-13 13:19:13 +0500

    MasterUpdateValidator Image can be null

--- src/Profex.Persistance/Dtos/User1/User1UpateDto.cs
+++ src/Profex.Persistance/Dtos/User1/User1UpateDto.cs
@@ -7,8 +7,5 @@ public class User1UpateDto
     public string FirstName { get; set; } = String.Empty;
     public string LastName { get; set; } = String.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
-    //public string PhoneNumberConfirmed { get; set; } = string.Empty;
-    public IFormFile ImagePath { get; set; } = null;
-    //public string PasswordHash { get; set; } = string.Empty;
-    //public string Salt { get; set; } = string.Empty;
+    public IFormFile? ImagePath { get; set; } = null;
 }
