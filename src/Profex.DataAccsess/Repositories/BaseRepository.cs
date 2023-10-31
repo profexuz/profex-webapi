fe77370546bcfa13feda60f5139c22e993566593
Temurbek <96979969+temurkodirov@users.noreply.github.com>
2023-10-31 17:13:00 +0500

    new database

--- src/Profex.DataAccsess/Repositories/BaseRepository.cs
+++ src/Profex.DataAccsess/Repositories/BaseRepository.cs
@@ -10,6 +10,6 @@ public class BaseRepository
     {
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
-        _connection = new NpgsqlConnection("Host=db-postgresql-nyc3-62486-do-user-14993247-0.c.db.ondigitalocean.com; Port=25060; Database=gettalim-db; User Id=doadmin; Password=AVNS_6X-S9VpP5KFVSzOvFXt;");
+        _connection = new NpgsqlConnection("Host=db-postgresql-nyc3-62486-do-user-14993247-0.c.db.ondigitalocean.com; Port=25060; Database=profex-db; User Id=doadmin; Password=AVNS_6X-S9VpP5KFVSzOvFXt;");
     }
 }
